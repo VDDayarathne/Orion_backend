94b5c5dcc2596d780d8369508af0e0e5ee1c6663
VDDayarathne <157834574+VDDayarathne@users.noreply.github.com>
2025-02-11 22:07:47 +0530

    Add files via upload

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
 from . import views
 from .views import make_image
+from .views import make_text
+from django.contrib.auth import views as auth_views
+from .views import user_logout
+from .views import get_user_scores, get_image_scores, get_text_scores
+
 
 
 urlpatterns = [
     path('', views.make_image, name='make_image'),
     path('make_image/', make_image, name='make_image'),
+    path('make_text/', make_text, name='make_text'),
+    path('login/', views.login_view, name='login'),
+    path("logout/", user_logout , name="logout"),
+    path('register/', views.register, name='register'),
+    path('get_user_scores/', get_user_scores, name='get_user_scores'),
+    path('get_image_scores/', get_image_scores, name='get_image_scores'),
+    path('get_text_scores/', get_text_scores, name='get_text_scores'),
+    
 ]
